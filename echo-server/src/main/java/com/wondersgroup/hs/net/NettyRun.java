760dfe550bc0e599bb4aaee68f37694d84fff882
zarra <zarrax@163.com>
2015-09-30 16:29:31 +0800

    修改了pom

--- echo-server/src/main/java/com/wondersgroup/hs/net/NettyRun.java
+++ echo-server/src/main/java/com/wondersgroup/hs/net/NettyRun.java
@@ -20,60 +20,60 @@ public class NettyRun {
     static Logger logger = LoggerFactory.getLogger(NettyRun.class);
 
     public static void main( String[] args ) throws IOException, InterruptedException, URISyntaxException {
-        //NettyServer service = new NettyServer();
-        //service.setFactory(new EchoHandlerFactory());
+        NettyServer service = new NettyServer();
+        service.setFactory(new EchoHandlerFactory());
 
-        //service.start();
-
-
-        HandlerFactory factory = new HandlerFactory() {
-            @Override
-            public ChannelHandlerAdapter[] createHandler() {
-                return new ChannelHandlerAdapter[]{
-                        new ChannelInboundHandlerAdapter(){
-                            @Override
-                            public void channelActive(ChannelHandlerContext ctx) throws Exception {
-                                super.channelActive(ctx);
-                            }
-
-                            @Override
-                            public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-                                super.channelInactive(ctx);
-                            }
-
-                            @Override
-                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                //super.channelRead(ctx, msg);
-                                if (msg instanceof String){
-                                    logger.info(""+msg);
-                                }
-                            }
-                        }
-                };
-            }
-        };
-
-        NettyWebSocketClient webSocketClient = new NettyWebSocketClient();
-        webSocketClient.setFactory(factory);
-        webSocketClient.setShowLog(false);
-        webSocketClient.connect(new URI("ws://localhost:8088/echo"));
-
-        Channel channel = webSocketClient.getChannel();
-
-//        WebSocketClientHandler handler = webSocketClient.getHandler();
-//        handler.handshakeFuture().sync();
+        service.start();
 
 
-        //Thread.sleep(1000L);
-        //service.stop();
-        Thread.sleep(1000L);
-
-        channel.writeAndFlush("Hello websocket");
-
-        Thread.sleep(1000L);
-
-
-        webSocketClient.close();
+//        HandlerFactory factory = new HandlerFactory() {
+//            @Override
+//            public ChannelHandlerAdapter[] createHandler() {
+//                return new ChannelHandlerAdapter[]{
+//                        new ChannelInboundHandlerAdapter(){
+//                            @Override
+//                            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+//                                super.channelActive(ctx);
+//                            }
+//
+//                            @Override
+//                            public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+//                                super.channelInactive(ctx);
+//                            }
+//
+//                            @Override
+//                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//                                //super.channelRead(ctx, msg);
+//                                if (msg instanceof String){
+//                                    logger.info(""+msg);
+//                                }
+//                            }
+//                        }
+//                };
+//            }
+//        };
+//
+//        NettyWebSocketClient webSocketClient = new NettyWebSocketClient();
+//        webSocketClient.setFactory(factory);
+//        webSocketClient.setShowLog(false);
+//        webSocketClient.connect(new URI("ws://localhost:8088/echo"));
+//
+//        Channel channel = webSocketClient.getChannel();
+//
+////        WebSocketClientHandler handler = webSocketClient.getHandler();
+////        handler.handshakeFuture().sync();
+//
+//
+//        //Thread.sleep(1000L);
+//        //service.stop();
+//        Thread.sleep(1000L);
+//
+//        channel.writeAndFlush("Hello websocket");
+//
+//        Thread.sleep(1000L);
+//
+//
+//        webSocketClient.close();
 
 
     }
